f6c9ec58eafee1f93aff42c3b428dfd5ce8ef9e4
unknown <joel.buti97@gmail.com>
2024-01-23 16:05:10 +0200

    update

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ router.post('/login', (req,res)=>{
     var mobileNo = req.body.username
     var sql = `SELECT * FROM USER WHERE mobileNo =?`   //"0123456789"
     connection.query(sql, [mobileNo],(err, results)=>{
-        if(err) console(err)
+        if(err) console.log(err)
         console.log(results.length);
         if(results.length > 0){
             if(results[0].password == req.body.password){
